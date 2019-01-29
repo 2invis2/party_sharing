d68e5627368113af75f72ec33eb55c25d2038ae4
Олег <invisible6462@gmail.com>
2019-01-29 16:43:16 +0700

    Необходимые файлы из шаблона
    Добавлен Retrofit
    Проект структурирован под Cleen

---
+++ PartySharing/app/src/main/java/com/cft/shift/partysharing/partysharing/features/MvpView.java
@@ -0,0 +1,10 @@
+package com.cft.shift.partysharing.partysharing.features;
+
+/**
+ * Created: samokryl
+ * Date: 02.07.18
+ * Time: 0:08
+ */
+
+public interface MvpView {
+}
---
+++ PartySharing/app/src/main/java/com/cft/shift/partysharing/partysharing/features/feed/presentation/FeedActivity.java
@@ -0,0 +1,49 @@
+package com.cft.shift.partysharing.partysharing.features.feed.presentation;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.cft.shift.partysharing.partysharing.R;
+
+public class FeedActivity extends BaseActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_feed:
+                    mTextMessage.setText(R.string.title_feed);
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_create:
+                    mTextMessage.setText(R.string.title_create);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
